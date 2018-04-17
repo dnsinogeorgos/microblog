acd287e60669f783d188d3e442a2201b92e3ce81
Dimitrios-Nikolaos Sinogeorgos <dnsinogeorgos@protonmail.ch>
2018-04-17 17:46:04 +0300

    add searchable mixin class plus some stuffs

--- app/__init__.py
+++ app/__init__.py
@@ -80,6 +80,3 @@ def create_app(config_class=Config):
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
-
-
-from app import models
